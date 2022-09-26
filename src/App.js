e5f7055669166f1752afd244f07ec751332a34b7
Abhinash8560 <66021401+Abhinash8560@users.noreply.github.com>
2022-09-26 23:10:10 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React,{useState} from "react";
+import "./style.css";
+
+export default function App() {
+
+ 
+  return (
+    <div className="App">
+ <button className="btn">buttton1</button>
+ <button className="btn">buttton2</button>
+ <button className="btn">buttton3</button>
+ <button className="btn">buttton4</button>
+ <button className="btn">buttton5</button>
+ <button className="btn">buttton6</button>
+
+ 
+    </div>
+  );
+}
